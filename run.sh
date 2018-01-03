91fb5d6726321603e762731dc8aa2f918b0ff902
kosta709 <kosta@codefresh.io>
2018-01-03 21:13:40 +0200

    changed to docker save/load

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname $0)
 BASE_DIR=/dind
 DIND_IMAGES_LIB_ETALON_DIR=${BASE_DIR}/images-lib-etalon
+DIND_IMAGES_LIB_ETALON_SAVE=${BASE_DIR}/images-lib-etalon.save.tar
 mkdir -p ${DIND_IMAGES_LIB_ETALON_DIR}
 
 DIND_IMAGES_LIBS_DIR=${BASE_DIR}/images-libs
@@ -39,8 +40,9 @@ trap debug_trap SIGUSR1
 sighup_trap(){
    echo -e "\n ############## $(date) - SIGHUP received ####################"
    create_etalon
-   rsync_images_libs
+   sync_images_libs
    delete_extra_images_libs
+   ensure_no_docker_running
 }
 trap sighup_trap SIGHUP
 
@@ -52,7 +54,76 @@ sigusr2_trap(){
 }
 trap sigusr2_trap SIGUSR2
 
+start_docker_on_data_root(){
+   # Starting dockerd with known data-root and pidfile
+   local DOCKERD_DATA_ROOT=$1
+   local DOCKERD_PID_FILE=$2
+   local DOCKER_SOCK_FILE=$3
 
+   local DOCKER_PID
+   local DOCKER_HOST="unix://${DOCKER_SOCK_FILE}"
+   echo -e "  ---- $(date) start_docker_on_data_root on ${DOCKERD_DATA_ROOT} "
+   [[ -z "${DOCKERD_DATA_ROOT}" ]] && echo "Error: DOCKERD_DATA_ROOT 1 param is empty" && return 1
+   [[ -z "${DOCKERD_PID_FILE}" ]] && echo "Error: DOCKERD_PID_FILE 2 param is empty" && return 1
+   [[ -z "${DOCKER_SOCK_FILE}" ]] && echo "Error: DOCKER_SOCK_FILE 3 param is empty" && return 1
+
+    echo "      Starting dockerd --data-root=${DOCKERD_DATA_ROOT} --pidfile=${DOCKERD_PID_FILE} --host=${DOCKER_HOST} $DOCKERD_PARAMS "
+    dockerd --data-root=${DOCKERD_DATA_ROOT} --pidfile=${DOCKERD_PID_FILE} --host=${DOCKER_HOST} $DOCKERD_PARAMS <&- &
+
+    local CNT=0
+    while ! test -f ${DOCKERD_PID_FILE} || test -z "$(cat ${DOCKERD_PID_FILE})" || ! docker ps
+    do
+      echo "      $(date) - Waiting for docker to start"
+      sleep 2
+      (( CNT++ ))
+      if (( CNT > 30 )); then
+        echo "Error: failed to start dockerd"
+        DOCKER_PID=$(cat ${DOCKERD_PID_FILE})
+        if [[ -n "${DOCKER_PID}" ]]; then
+           kill -9 "${DOCKER_PID}"
+        fi
+        return 1
+      fi
+    done
+    echo "       Successfully started dockerd with --data-root ${DIND_IMAGES_LIB_ETALON_DIR} --pidfile ${DOCKERD_PID_FILE} "
+}
+
+kill_docker_by_pid(){
+    local DOCKER_PID=$1
+    echo -e "\n------- $(date) \nKilling dockerd DOCKER_PID = ${DOCKER_PID}"
+    [[ -z "${DOCKER_PID}" ]] && echo "Error: DOCKER_PID 1 param is empty" && return 1
+
+    kill ${DOCKER_PID}
+    echo "Waiting for dockerd to exit ..."
+    local CNT=0
+    while ps -p ${DOCKER_PID}
+    do
+       (( CNT++ ))
+       echo ".... dockerd is still running - $(date)"
+       if (( CNT >= 60 )); then
+         echo "Killing dockerd"
+         kill -9 ${DOCKER_PID}
+         break
+       fi
+       sleep 1
+    done
+}
+
+ensure_no_docker_running(){
+    echo "ensure_no_docker_running - Waiting for dockerd to exit ..."
+    local CNT=0
+    while pgrep -l docker
+    do
+       CNT=$(expr ${CNT} '+' 1)
+       echo ".... old dockerd is still running - $(date)"
+       if [[ ${CNT} -ge 10 ]]; then
+         echo "Killing old dockerd"
+         pkill -9 docker
+         break
+       fi
+       sleep 0.5
+    done
+}
 
 create_etalon(){
     echo -e "---- Creating etalon image lib dir"
@@ -60,54 +131,72 @@ create_etalon(){
       echo "RECREATE_ETALON = ${RECREATE_ETALON} - deleting existing ${DIND_IMAGES_LIB_ETALON_DIR} "
       rm -rf "${DIND_IMAGES_LIB_ETALON_DIR}"
       mkdir -p "${DIND_IMAGES_LIB_ETALON_DIR}"
+      rm -fv ${DIND_IMAGES_LIB_ETALON_SAVE}
     fi
 
-    echo "Starting dockerd --data-root = ${DIND_IMAGES_LIB_ETALON_DIR} $DOCKERD_PARAMS "
-    dockerd --data-root "${DIND_IMAGES_LIB_ETALON_DIR}" $DOCKERD_PARAMS &>/tmp/dockerd.log <&- &
+    local DOCKERD_NUM=${RANDOM}
+    local DOCKER_PID_FILE=/var/run/docker-${DOCKERD_NUM}.pid
+    local DOCKER_SOCK_FILE=/var/run/docker-${DOCKERD_NUM}.sock
+    rm -f ${DOCKER_PID_FILE} ${DOCKER_SOCK_FILE}
+    echo "calling start_docker_on_data_root ${DIND_IMAGES_LIB_ETALON_DIR} ${DOCKER_PID_FILE} "
+    start_docker_on_data_root ${DIND_IMAGES_LIB_ETALON_DIR} ${DOCKER_PID_FILE} ${DOCKER_SOCK_FILE} || return 1
 
-    while ! test -f /var/run/docker.pid || test -z "$(cat /var/run/docker.pid)" || ! docker ps
-    do
-      echo "$(date) - Waiting for docker to start"
-      sleep 2
-    done
+    local DOCKER_PID=$(cat ${DOCKER_PID_FILE})
+    [[ -z "${DOCKER_PID}" ]] && echo "Error DOCKER_PID is empty - docker failed to start" && return 1
 
-    DOCKER_PID=$(cat /var/run/docker.pid)
-    echo "Docker daemon has been started with DOCKER_PID = ${DOCKER_PID} "
+    local DOCKER_HOST="unix://${DOCKER_SOCK_FILE}"
+    echo "Docker daemon has been started with DOCKER_PID = ${DOCKER_PID} and DOCKER_HOST="unix://${DOCKER_SOCK_FILE}""
 
     if [[ -f "${IMAGES_DELETE_LIST}" ]]; then
         echo -e "\n-------  $(date) \nDeleting images from IMAGES_DELETE_LIST = ${IMAGES_DELETE_LIST} "
         cat ${IMAGES_DELETE_LIST} | while read image
         do
           echo "    Deleting image ${image} "
-          docker rmi -f "${image}"
+          docker -H ${DOCKER_HOST} rmi -f "${image}"
         done
     fi
 
-    echo "\n-------  $(date)  \nPulling images from IMAGES_PULL_LIST = ${IMAGES_PULL_LIST} "
+    echo -e "\n-------  $(date)  \nPulling images from IMAGES_PULL_LIST = ${IMAGES_PULL_LIST} "
+    local IMAGES_PULL_SAVE=""
     cat ${IMAGES_PULL_LIST} | while read image
     do
       echo "Pulling image ${image} "
-      docker pull "${image}"
+      docker -H ${DOCKER_HOST} pull "${image}" && \
+      IMAGES_PULL_SAVE="${IMAGES_PULL_SAVE} ${image}"
     done
 
-    echo -e "\n------- $(date) \nKilling dockerd DOCKER_PID = ${DOCKER_PID}"
-    kill ${DOCKER_PID}
-    echo "Waiting for dockerd to exit ..."
-    local CNT=0
-    while pgrep -l docker
-    do
-       (( CNT++ ))
-       echo ".... dockerd is still running - $(date)"
-       if (( CNT >= 60 )); then
-         echo "Killing dockerd"
-         pkill -9 docker
-         break
-       fi
-       sleep 1
-    done
+    echo -e "\n     --- $(date) - docker save -o ${DIND_IMAGES_LIB_ETALON_SAVE} ${IMAGES_PULL_SAVE}"
+    docker -H ${DOCKER_HOST} save -o ${DIND_IMAGES_LIB_ETALON_SAVE} ${IMAGES_PULL_SAVE}
+
+    kill_docker_by_pid ${DOCKER_PID}
 }
 
-rsync_images_libs(){
+load_images_lib(){
+    local IMAGES_LIB_DIR="${1}"
+    echo -e "\n------- $(date) \nload_images_lib on IMAGES_LIB_DIR = ${IMAGES_LIB_DIR}"
+
+    [[ -z "${IMAGES_LIB_DIR}" ]] && echo "Error: IMAGES_LIB_DIR 1 param is empty" && return 1
+    mkdir -p ${IMAGES_LIB_DIR}
+
+    local DOCKERD_NUM=${RANDOM}
+    local DOCKER_PID_FILE=/var/run/docker-${DOCKERD_NUM}.pid
+    local DOCKER_SOCK_FILE=/var/run/docker-${DOCKERD_NUM}.sock
+
+
+    rm -f ${DOCKER_PID_FILE} ${DOCKER_SOCK_FILE}
+
+    echo "calling start_docker_on_data_root ${IMAGES_LIB_DIR} ${DOCKER_PID_FILE} "
+    start_docker_on_data_root ${IMAGES_LIB_DIR} ${DOCKER_PID_FILE} ${DOCKER_SOCK_FILE} || return 1
+
+    local DOCKER_PID=$(cat ${DOCKER_PID_FILE})
+    local DOCKER_HOST="unix://${DOCKER_SOCK_FILE}"
+
+    docker -H ${DOCKER_HOST} load < ${DIND_IMAGES_LIB_ETALON_SAVE}
+
+    kill_docker_by_pid ${DOCKER_PID}
+}
+
+sync_images_libs(){
     local DEST_DIR
     local DEST_DIR_TMP
     for ii in $(seq -w ${DESIRED_DOCKER_LIB_NUMBER})
@@ -119,7 +208,7 @@ rsync_images_libs(){
           echo -e "\n    -- $(date) -- Syncing ${DEST_DIR}"
           rm -rf ${DEST_DIR_TMP}
           mv ${DEST_DIR} ${DEST_DIR_TMP} && \
-          rsync -aW --no-compress --delete ${DIND_IMAGES_LIB_ETALON_DIR}/ ${DEST_DIR_TMP}/ && \
+          load_images_lib ${DEST_DIR_TMP} && \
           mv ${DEST_DIR_TMP} ${DEST_DIR}
       fi
     done
@@ -143,8 +232,9 @@ delete_extra_images_libs(){
 echo "Entering $0 at $(date) "
 
 create_etalon
-rsync_images_libs
+sync_images_libs
 delete_extra_images_libs
+ensure_no_docker_running
 
 echo -e "\n------- $(date) \nStarting to synchronize images lib directories ${DIND_IMAGES_LIBS_DIR}/ with etalon"
 
@@ -176,7 +266,8 @@ do
             echo -e "\n   -- $(date) -- Creating image lib dir ${DEST_DIR}"
             rm -rf ${DEST_DIR_TMP}
             mkdir -p ${DEST_DIR_TMP} && \
-            cp -a ${DIND_IMAGES_LIB_ETALON_DIR}/ ${DEST_DIR_TMP}/ && \
+            load_images_lib ${DEST_DIR_TMP} && \
+            ensure_no_docker_running && \
             mv ${DEST_DIR_TMP} ${DEST_DIR} && \
             echo -e "       $(date) - Successfully created ${DEST_DIR}"
           fi
